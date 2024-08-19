37f6654e43f7c5dcfd5ef6a1951094d9b2647206
East of Eden <azazal.combs@gmail.com>
2024-08-20 00:17:34 +0300

    project 10 commit 5

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
--- src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
